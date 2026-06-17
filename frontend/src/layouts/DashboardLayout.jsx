1e3dd399175fe27a74843c8a129419c8f1dd5329
Mayura Pabasara <mayurapabasara143@gmail.com>
2026-06-17 09:41:13 +0530

    modified dashBoardLayout.jsx

--- frontend/src/layouts/DashboardLayout.jsx
+++ frontend/src/layouts/DashboardLayout.jsx
@@ -1,4 +1,4 @@
-import Sidebar from "../components/layout/sidebar";
+import Sidebar from "../components/layout/Sidebar";
 import Topbar from "../components/layout/Topbar";
 
 export default function DashboardLayout({ children }) {
