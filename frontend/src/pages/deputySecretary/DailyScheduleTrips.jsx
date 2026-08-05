b0fa4172390559278f38a2af65a37d5e85d96c75
Mayura Pabasara <mayurapabasara143@gmail.com>
2026-08-05 14:25:44 +0530

    add DailyScheduleTrips component with basic layout

---
+++ frontend/src/pages/deputySecretary/DailyScheduleTrips.jsx
@@ -0,0 +1,9 @@
+import DashboardLayout from "../../layouts/DashboardLayout";
+
+export default function DailyScheduleTrips() {
+    return (
+        <DashboardLayout>
+            <div className="min-h-screen bg-slate-50 p-6">Daily Schedule Trips</div>
+        </DashboardLayout>
+    );
+}
