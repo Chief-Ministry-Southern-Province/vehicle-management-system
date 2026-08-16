0a2effc02fe075fc39d5da8891332c76e403f98f
Mayura Pabasara <mayurapabasara143@gmail.com>
2026-08-16 23:33:09 +0530

    feat: add FuleAnalysis component with basic layout

---
+++ frontend/src/pages/deputySecretary/FuleAnalysis.jsx
@@ -0,0 +1,11 @@
+import DashboardLayout from "../../layouts/DashboardLayout";
+
+export default function FuleAnalysis() {
+  return (
+    <div>
+        <DashboardLayout>
+                  <h1>Fuel Analysis</h1>
+        </DashboardLayout>
+    </div>
+  )
+}
